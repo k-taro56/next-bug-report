3c8f8e96488aab139e02a64a8079506290c40760
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-10-19 14:16:12 +0900

    add sample page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,11 @@
+export const generateStaticParams = () => [];
+
+const Sample = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24">
+            <h1>Sample</h1>
+        </main>
+    );
+}
+
+export default Sample;
